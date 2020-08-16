5640d23a9179f67440028aebf4fcc42ecbe34716
RicardoArquimedes <richardcqt28@gmail.com>
2020-08-16 15:29:44 +0000

    creating our own _strlen & _strcpy

---
+++ _strcpy.c
@@ -0,0 +1,23 @@
+#include "shell.h"
+
+/**
+ * _strcpy - copies the string pointed to by src into dest
+ * @dest: destination of the copy
+ * @src: source of the copy
+ *
+ * Return: char pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (*(src + i) != '\0')
+	{
+		*(dest + i) = *(src + i);
+		++i;
+	}
+	*(dest + i) = *(src + i);
+
+	return (dest);
+}
