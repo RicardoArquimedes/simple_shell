2865f43f2a833c2c75336227b0d627c16d1703e3
Jocelyn Vega <jossvega103@gmail.com>
2020-08-20 03:30:42 +0000

    Ass handler error and -1 in getline

--- _getline_command.c
+++ _getline_command.c
@@ -12,7 +12,7 @@ char *_getline_command(void)
 	if (isatty(STDIN_FILENO))
 		write(STDOUT_FILENO, "$ ", 2);
 
-	if (getline(&lineptr, &charter_user, stdin) == 1)
+	if (getline(&lineptr, &charter_user, stdin) == -1)
 	{
 		free(lineptr);
 		return (NULL);
