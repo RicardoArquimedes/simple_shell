aee4a7952504c2ab7f4dc946cf464ce54749d65c
Jocelyn Vega <jossvega103@gmail.com>
2020-08-20 03:24:28 +0000

    Modify promp getline

--- _getline_command.c
+++ _getline_command.c
@@ -10,7 +10,7 @@ char *_getline_command(void)
 	size_t charter_user = 0;
 
 	if (isatty(STDIN_FILENO))
-		write(STDOUT_FILENO, "#cisfun$ ", 10);
+		write(STDOUT_FILENO, "$ ", 2);
 
 	if (getline(&lineptr, &charter_user, stdin) == 1)
 	{
